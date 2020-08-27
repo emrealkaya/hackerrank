5892faaece8ca8c9ac8c0770582f25ca5c81fcd7
emrealkaya <emre.alkayaa@gmail.com>
2020-08-27 21:38:15 +0300

    my hackerrank Java Stdin And Stdout solution

---
+++ src/JavaStdinAndStdout/Solution.java
@@ -0,0 +1,17 @@
+package JavaStdinAndStdout;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int myFirstNumber = scanner.nextInt();
+        int mySecondNumber = scanner.nextInt();
+        int myThirdNumber = scanner.nextInt();
+        System.out.println(myFirstNumber);
+        System.out.println(mySecondNumber);
+        System.out.println(myThirdNumber);
+
+    }
+}
