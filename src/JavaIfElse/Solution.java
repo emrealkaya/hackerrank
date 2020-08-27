de50390e1cf339f2bed0c11a2c1d8e0085c29038
emrealkaya <emre.alkayaa@gmail.com>
2020-08-27 22:21:54 +0300

    my hackerrank Java If-Else solution

---
+++ src/JavaIfElse/Solution.java
@@ -0,0 +1,24 @@
+package JavaIfElse;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        if (n % 2 == 1) {
+            System.out.println("Weird");
+        } else {
+            if (n >= 2 && n <= 5) {
+                System.out.println("Not Weird");
+            } else if (n >= 5 && n <= 20) {
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
+            }
+        }
+        scanner.close();
+    }
+}
